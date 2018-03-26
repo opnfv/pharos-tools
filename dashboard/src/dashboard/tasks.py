8ff22a49625c6879026bc77d07a6fcc7430a0266
Sawyer Bergeron <sbergeron@iol.unh.edu>
2018-03-26 17:07:55 -0400

    Implement Booking Cleanup Task
    Jira: PHAROS-344
    
    Two new settings in config.env allow setting
    	1) how "stale" an expired booking can get before being culled and
    	2) how many expired bookings are kept around before culling
    If either condition is met, expired bookings will be deleted until
    both are false.
    
    Change-Id: I6b091e93c6fe5d47b75ebf5325b717fa898c773c
    Signed-off-by: Sawyer Bergeron <sbergeron@iol.unh.edu>

--- dashboard/src/dashboard/tasks.py
+++ dashboard/src/dashboard/tasks.py
@@ -7,16 +7,34 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
 
-
+import datetime
+import time
 from datetime import timedelta
 
 from celery import shared_task
 from django.utils import timezone
+from django.conf import settings
+from booking.models import Booking
 
 from jenkins.models import JenkinsStatistic
-
 
 @shared_task
 def database_cleanup():
     now = timezone.now()
     JenkinsStatistic.objects.filter(timestamp__lt=now - timedelta(weeks=4)).delete()
+
+def booking_cleanup():
+    expire_time = datetime.timedelta(days=int(settings.BOOKING_EXP_TIME))
+    expire_number = int(settings.BOOKING_MAX_NUM)
+    expired_set = Booking.objects.filter(end__lte=timezone.now())
+    expired_count = len(expired_set)
+
+    for booking in Booking.objects.all():
+        if timezone.now() - booking.end > expire_time:
+            booking.delete()
+            expired_count = expired_count - 1
+
+    if expired_count > expire_number:
+        oldest = expired_set.order_by("end")[:expired_count-expire_number]
+        for booking in oldest:
+            booking.delete()
