16464ee01efface79a085b695dd86f9b61823206
Sawyer Bergeron <sbergeron@iol.unh.edu>
2019-06-06 12:45:32 -0400

    redirect to booking detail on creation
    Change-Id: I4e27f6a4a64314639b9ac83750b5b6add069399b
    Signed-off-by: Sawyer Bergeron <sbergeron@iol.unh.edu>

--- dashboard/src/workflow/models.py
+++ dashboard/src/workflow/models.py
@@ -383,6 +383,7 @@ class Repository():
     # each of these is the end result of a different workflow.
     HAS_RESULT = "whether or not workflow has a result"
     RESULT_KEY = "key for target index that result will be put into in parent"
+
     RESULT = "result object from workflow"
 
     def get_child_defaults(self):
@@ -490,6 +491,9 @@ class Repository():
             pass
         JobFactory.makeSnapshotTask(image, booking, host)
 
+        self.el[self.RESULT] = image
+        self.el[self.HAS_RESULT] = True
+
     def make_generic_resource_bundle(self):
         owner = self.el[self.SESSION_USER]
         if self.GRESOURCE_BUNDLE_MODELS in self.el:
@@ -556,6 +560,7 @@ class Repository():
             return "GRB no models given. CODE:0x0001"
 
         self.el[self.RESULT] = bundle
+        self.el[self.HAS_RESULT] = True
         return False
 
     def make_software_config_bundle(self):
@@ -667,6 +672,9 @@ class Repository():
         except Exception as e:
             return "BOOK, saving booking generated exception: " + str(e) + " CODE:0x0016"
 
+        self.el[self.RESULT] = booking
+        self.el[self.HAS_RESULT] = True
+
     def make_opnfv_config(self):
         opnfv_models = self.el[self.OPNFV_MODELS]
         config_bundle = self.el[self.SELECTED_CONFIG_BUNDLE]
@@ -713,10 +721,12 @@ class Repository():
             )
 
         self.el[self.RESULT] = opnfv_config
+        self.el[self.HAS_RESULT] = True
 
     def __init__(self):
         self.el = {}
         self.el[self.CONFIRMATION] = {}
         self.el["active_step"] = 0
+        self.el[self.HAS_RESULT] = False
         self.get_history = {}
         self.put_history = {}
--- dashboard/src/workflow/views.py
+++ dashboard/src/workflow/views.py
@@ -10,10 +10,12 @@
 
 from django.http import HttpResponse, HttpResponseGone
 from django.shortcuts import render
+from django.urls import reverse
 
 import uuid
 
 from workflow.workflow_manager import ManagerTracker, SessionManager
+from booking.models import Booking
 
 import logging
 logger = logging.getLogger(__name__)
@@ -29,17 +31,34 @@ def attempt_auth(request):
         return None
 
 
+def get_redirect_context(result, has_result):
+    if not has_result:
+        return {}
+
+    # need to get type of result, and switch on the type
+    # since has_result, result must be populated with a valid object
+    if isinstance(result, Booking):
+        return {
+                'redir_url': reverse('booking:booking_detail', kwargs={'booking_id':result.id} )
+        }
+    else:
+        return {'redir_url': '/'}
+
+
 def delete_session(request):
     manager = attempt_auth(request)
 
     if not manager:
         return HttpResponseGone("No session found that relates to current request")
 
-    if manager.pop_workflow():
+    success, has_result, result = manager.pop_workflow()
+
+    if success:
         return HttpResponse('')
     else:
         del ManagerTracker.managers[request.session['manager_session']]
-        return render(request, 'workflow/exit_redirect.html')
+        redirect_context = get_redirect_context(result, has_result)
+        return render(request, 'workflow/exit_redirect.html', redirect_context)
 
     try:
         del ManagerTracker.managers[request.session['manager_session']]
